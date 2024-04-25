ee1778eb068616282024a056209a81bde36f2eb6
jos3lo89 <gothangel21@hotmail.com>
2024-04-25 18:50:22 -0500

    Borrando backend con pnpm

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use((req, res) => {
-  res.json({ message: ["hola"] });
-});
-
-app.listen(3000, () => console.log("server on port 3000"));
